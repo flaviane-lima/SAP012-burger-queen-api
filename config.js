0cf3fee21f36818c90b36df4dec9cd455b499b1c
flaviane-lima <rflaviane41@gmail.com>
2024-04-10 22:52:42 -0300

    configuração para o banco de dados

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://127.0.0.1:27017/test';
-exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
-exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
-exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
+exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL;
+exports.secret = process.env.JWT_SECRET;
+exports.adminEmail = process.env.ADMIN_EMAIL;
+exports.adminPassword = process.env.ADMIN_PASSWORD;
+
+// const requirezzz('dotenv').config();

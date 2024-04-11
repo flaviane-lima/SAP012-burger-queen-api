1ef2d85fe4781affd6dad198cf9353e5a1eb2e92
flaviane-lima <rflaviane41@gmail.com>
2024-04-10 23:08:29 -0300

    criei a pasta de model com arquivo usuário para fazer a estrutura do schema

---
+++ model/User.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin'],
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+const User = model('users', userSchema);
+
+module.exports = { User };

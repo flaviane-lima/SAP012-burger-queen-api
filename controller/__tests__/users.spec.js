6447fa4da18fc63a5046be851ce30ffc29b2be99
flaviane-lima <rflaviane41@gmail.com>
2024-04-23 17:12:32 -0300

    refatorado o teste de user do controller.

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -1,20 +1,21 @@
 const UserServices = require('../../services/UserServices');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const {
   getUsers,
   getUserById,
-  createrUser,
+  createUser,
   updateUser,
   deleteUser,
 } = require('../users');
 
 jest.mock('../../services/UserServices', () => ({
-  find: jest.fn(),
-  findByID: jest.fn(),
-  create: jest.fn(),
-  findByIdAndUpdate: jest.fn(),
-  findByIdAndDelete: jest.fn(),
+  getUsers: jest.fn(),
+  getUserById: jest.fn(),
+  getUserByEmail: jest.fn(),
+  createUser: jest.fn(),
+  updateUser: jest.fn(),
+  deleteUser: jest.fn(),
 }));
 
 const req = {};
@@ -22,6 +23,10 @@ const res = {
   status: jest.fn().mockReturnThis(),
   json: jest.fn(),
 };
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('getUsers', () => {
   it('deve obter a coleção de usuário', async () => {
@@ -39,20 +44,20 @@ describe('getUsers', () => {
     ];
 
     // definindo o retorno do mock
-    getUsers.mockResolvedValue(mockUsers);
+    UserServices.getUsers.mockResolvedValue(mockUsers);
 
-    const users = await getUsers(req, res);
+    await getUsers(req, res);
 
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith(mockUsers);
-    expect(users.length).toBe(2);
+    // expect(users.length).toBe(2);
   });
 
   it('deve retornar 500 e uma mensagem de erro se ocorrer um erro', async () => {
     const errorMessage = 'Erro ao buscar usuários';
 
     // Definindo o retorno do mock
-    UserServices.find.mockRejectedValue(new Error(errorMessage));
+    UserServices.getUsers.mockRejectedValue(new Error(errorMessage));
 
     await getUsers(req, res);
 
@@ -68,12 +73,9 @@ describe(' getUserById', () => {
       email: 'admin@localhost.com',
       password: '$2b$10$bYYW6Wugx1p4nvhV10dGMOOu3WYvliAtETIOCQjQJcS5O1h01Qe.u',
       role: 'admin',
-      // createdAt:2024-04-09T19:01:55.813+00:00,
-      // updatedAt:2024-04-09T19:01:55.813+00:00,
-      __v: 0,
     };
 
-    UserServices.find.mockResolvedValue(mockUsers);
+    UserServices.getUserById.mockResolvedValue(mockUsers);
     const req = {
       params: { uid: '661590a33f7a3dbdf69788fa' },
     };
@@ -88,102 +90,95 @@ describe(' getUserById', () => {
     const errorMessage = 'Erro ao buscar usuários';
 
     // Definindo o retorno do mock
-    UserServices.findByID.mockRejectedValue(new Error(errorMessage));
-
+    UserServices.getUserById.mockRejectedValue(new Error(errorMessage));
+    const req = {
+      params: { uid: '661590a33f7a3dbdf69788fa' },
+    };
     await getUserById(req, res);
 
-    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.status).toHaveBeenCalledWith(500);
     expect(res.json).toHaveBeenCalledWith({ error: errorMessage });
   });
 });
 
-describe('createrUser', () => {
+describe('createUser', () => {
+  beforeEach(() => {
+    bcrypt.genSalt = jest.fn().mockResolvedValue('genSalt');
+    bcrypt.hashSync = jest.fn().mockResolvedValue('hashedPassword');
+  });
+
   it('deve ser criado um usuário', async () => {
     const mockUsers = {
-      _id: '661590a33f7a3dbdf69788fa',
+      // _id: '661590a33f7a3dbdf69788fa',
       email: 'admin@localhost.com',
       password: '$2b$10$bYYW6Wugx1p4nvhV10dGMOOu3WYvliAtETIOCQjQJcS5O1h01Qe.u',
       role: 'admin',
-      // createdAt:2024-04-09T19:01:55.813+00:00,
-      // updatedAt:2024-04-09T19:01:55.813+00:00,
-      __v: 0,
     };
-    UserServices.create.mockResolvedValue(mockUsers); // consulta o banco de dados.
+    UserServices.createUser.mockResolvedValue(mockUsers); // consulta o banco de dados.
+    UserServices.getUserByEmail.mockResolvedValue(null);
     const req = {
       body: { email: 'admin@localhost.com', password: '12345', role: 'admin' },
     };
-
-    await createrUser(req, res);
+    await createUser(req, res);
 
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith({ data: mockUsers, status: 'sucesso' });
   });
   it('deve conter o email', async () => {
-    const mockEmail = {
-      email: 'admin@localhost.com',
-      password: 12345,
-      role: 'admin',
-    };
-    UserServices.create.mockResolvedValue(mockEmail);
+    UserServices.createUser.mockResolvedValue();
     const req = {
-      body: { email: 'admin@localhost.com', password: 12345, role: 'admin' },
+      body: { password: 12345, role: 'admin' },
     };
 
-    await createrUser(req, res);
+    await createUser(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith('usuário precisa do email, da senha e do role');
+    expect(res.json).toHaveBeenCalledWith({ error: 'usuário precisa do email, da senha e do role' });
   });
 });
 
 describe('updateUser', () => {
-  it('precisa atualizar o suário', async () => {
+  it('precisa atualizar o usário', async () => {
     const mockUsers = {
-      _id: '661590a33f7a3dbdf69788fa',
       email: 'admin@localhost.com',
       password: '$2b$10$bYYW6Wugx1p4nvhV10dGMOOu3WYvliAtETIOCQjQJcS5O1h01Qe.u',
       role: 'admin',
-      // createdAt:2024-04-09T19:01:55.813+00:00,
-      // updatedAt:2024-04-09T19:01:55.813+00:00,
-      __v: 0,
     };
-    UserServices.findByIdAndUpdate.mockResolvedValue(mockUsers); // consulta o banco de dados.
+    UserServices.updateUser.mockResolvedValue(mockUsers); // consulta o banco de dados.
     const req = {
-      params: { email: 'admin@localhost.com', password: '12345', role: 'admin' },
+      params: { id: 'user_id' },
+      body: { email: 'admin@localhost.com', password: '12345', role: 'admin' },
     };
 
     await updateUser(req, res);
-
+    expect(UserServices.updateUser).toHaveBeenCalledWith(req.params.id, {
+      email: req.body.email,
+      password: req.body.password,
+      role: req.body.role,
+    });
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith(mockUsers);
   });
-  it('deve retornar 400 se o usuário não for encontrado', async () => {
-    const mockEmail = {
-      email: 'admin@localhost.com',
-      password: 12345,
-      role: 'admin',
-    };
-    UserServices.findByIdAndUpdate.mockResolvedValue(mockEmail); // (null) deveria ser esse?
+  it('deve retornar 400 se o usuário não for encontrado', async () => { 
+    UserServices.updateUser.mockResolvedValue(null); // (null) deveria ser esse?
     const req = {
-      body: { email: 'admin@localhost.com', password: 12345, role: 'admin' },
+      params: { id: 'user_id' },
+      body: { email: 'admin@localhost.com', password: '12345', role: 'admin' },
     };
     await updateUser(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith('precisa do usuário'); // ({ error: 'precisa do usuário' })?
+    expect(res.json).toHaveBeenCalledWith({ error: 'precisa do usuário' });
   });
 
-  it('deve ter o usuário', async () => {
-    const mockEmail = {
-      email: 'admin@localhost.com',
-      password: 12345,
-      role: 'admin',
-    };
-    UserServices.findByIdAndUpdate.mockResolvedValue(mockEmail);
+  it('deve retornar 500 se houver um erro', async () => {
+    const errorMessage = 'erro ao atualizar o usuário';
+    UserServices.updateUser.mockRejectedValue(new Error(errorMessage));
     const req = {
-      body: { email: 'admin@localhost.com', password: 12345, role: 'admin' },
+      params: { id: 'user_id' },
+      body: { email: 'admin@localhost.com', password: '12345', role: 'admin' },
     };
     await updateUser(req, res);
     expect(res.status).toHaveBeenCalledWith(500);
-    expect(res.json).toHaveBeenCalledWith('precisa do usuário');
+    expect(res.json).toHaveBeenCalledWith({ error: errorMessage });
   });
 });
 
@@ -195,22 +190,16 @@ describe('deleteUser', () => {
       password: 12345,
       role: 'admin',
     };
-    UserServices.findByIdAndDelete.mockResolvedValue(mockUser);
+    UserServices.deleteUser.mockResolvedValue(mockUser);
     const req = {
       params: { uid: '661590a33f7a3dbdf69788fa' },
     };
     await deleteUser(req, res);
     expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith('precisa do usuário'); // aqui tenho que colocar mockUser?
+    expect(res.json).toHaveBeenCalledWith(mockUser); // aqui tenho que colocar mockUser?
   });
   it('não deve deletar o usuário se ele não existir', async () => {
-    // preciso deste mock?
-    const mockEmail = {
-      email: 'admin@localhost.com',
-      password: 12345,
-      role: 'admin',
-    };
-    UserServices.findByIdAndDelete.mockResolvedValue(mockEmail);
+    UserServices.deleteUser.mockResolvedValue(null);
     const req = {
       params: { email: 'admin@localhost.com', password: 12345, role: 'admin' },
     };
@@ -221,23 +210,13 @@ describe('deleteUser', () => {
 
   it('deve retornar 500 se ocorrer um erro', async () => {
     // preciso deste mock?
-    // const errorMessage = 'Erro ao deletar usuário';
-    const mockEmail = {
-      email: 'admin@localhost.com',
-      password: 12345,
-      role: 'admin',
-    };
-    // UserServices.findByIdAndDelete.mockRejectedValue(new Error(errorMessage)); // Simula um erro?
-    UserServices.findByIdAndDelete.mockResolvedValue(mockEmail);
-    // const req = {
-      //params: { uid: '661590a33f7a3dbdf69788fa' },
-  //};?
+    const errorMessage = 'Erro ao deletar usuário';
+    UserServices.deleteUser.mockRejectedValue(new Error(errorMessage)); // Simula um erro?
     const req = {
       params: { email: 'admin@localhost.com', password: 12345, role: 'admin' },
     };
     await deleteUser(req, res);
     expect(res.status).toHaveBeenCalledWith(500);
-    // expect(res.json).toHaveBeenCalledWith({ error: errorMessage });
-    expect(res.json).toHaveBeenCalledWith({ error: 'precisa do usuário' });
+    expect(res.json).toHaveBeenCalledWith({ error: errorMessage });
   });
 });

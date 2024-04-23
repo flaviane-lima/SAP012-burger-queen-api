8962759f4ce0b7adb3fac323cc6fa7d4be30c87a
flaviane-lima <rflaviane41@gmail.com>
2024-04-23 17:16:41 -0300

    alterei para ficar padrão os nomes das funções do createrUser e updateUser

--- services/UserServices.js
+++ services/UserServices.js
@@ -6,8 +6,8 @@ exports.getUserById = async (uid) => UserModel.findById(uid, '_id email role ');
 
 exports.getUserByEmail = async (email) => UserModel.findOne({ email }, '_id email role ');
 
-exports.createrUser = async (user) => UserModel.create(user);
+exports.createUser = async (user) => UserModel.create(user);
 
-exports.updateUse = async (id, users) => UserModel.findByIdAndUpdate(id, users);
+exports.updateUser = async (id, users) => UserModel.findByIdAndUpdate(id, users);
 
 exports.deleteUser = async (uid) => UserModel.findByIdAndDelete(uid);

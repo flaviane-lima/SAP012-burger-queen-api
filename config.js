66eadc20daa6f4c6f6dec98870b79a0eace623f9
flaviane-lima <rflaviane41@gmail.com>
2024-04-25 20:00:45 -0300

    mudei o nome do dbUrl

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-require('dotenv').config();
-
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL;
+exports.dbUrl = process.env.MONGODB_URI || process.env.DB_URL;
 exports.secret = process.env.JWT_SECRET;
 exports.adminEmail = process.env.ADMIN_EMAIL;
 exports.adminPassword = process.env.ADMIN_PASSWORD;

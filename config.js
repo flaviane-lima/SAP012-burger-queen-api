51681161bed38213b82cabc464e10616801b3560
flaviane-lima <rflaviane41@gmail.com>
2024-04-25 20:46:18 -0300

    alteração no arquivo

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGODB_URI || process.env.DB_URL;
-exports.secret = process.env.JWT_SECRET;
-exports.adminEmail = process.env.ADMIN_EMAIL;
-exports.adminPassword = process.env.ADMIN_PASSWORD;
-
-// const requirezzz('dotenv').config();
+exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb+srv://rflaviane41:@cluster0.tpihri1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&dbName=bq';
+exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
+exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost.com';
+exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
--- connect.js
+++ connect.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const config = require('./config');
 
 const { dbUrl } = config;
+console.info(dbUrl);
 async function connect() {
   try {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect('mongodb+srv://rflaviane41:GRorhoq5ZEMNJ6fm@cluster0.tpihri1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&dbName=bq');
   } catch (error) {
     console.error(`Erro: ${error}`);
   }
